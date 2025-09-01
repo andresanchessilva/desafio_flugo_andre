24827ada0103dfd4233cbb92f7a2b99587b78233
André Silva <andre.sanchessilva@gmail.com>
2025-09-01 03:54:33 -0300

    fix: remove-log

--- src/components/EmployeesDataGrid.tsx
+++ src/components/EmployeesDataGrid.tsx
@@ -90,8 +90,6 @@ export default function EmployeesDataGrid({
   sortModel,
   onSortModelChange,
 }: EmployeesDataGridProps) {
-  console.log(rows);
-
   return (
     <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
